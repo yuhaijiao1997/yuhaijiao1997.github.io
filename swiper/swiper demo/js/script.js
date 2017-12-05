5b90e6556a0d703f9c8890a5162c4af398f6f708
yuhaijiao1997 <1454370817@qq.com>
2017-12-05 09:43:15 +0800

    swiper

---
+++ swiper/swiper demo/js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+});
